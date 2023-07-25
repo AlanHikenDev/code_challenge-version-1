afd212a2cad4ce58b7767296683d3db3f10a1c3e
Alan Vasquez <alanvasloadev@gmail.com>
2023-07-25 09:40:52 -0600

    correcion verificacion de datos

--- spa-code-challenge/plugins/api.js
+++ spa-code-challenge/plugins/api.js
@@ -1,5 +1,5 @@
 export default function ({ $axios }, inject) {
-    let api_subdominio = 'http://code_challenge.test/' //aqui configuramos la url basica para axios
+    let api_subdominio = 'https://api-code.alandev.tech/' //aqui configuramos la url basica para axios
       
     console.log('host', api_subdominio)
     $axios.setBaseURL(api_subdominio)
